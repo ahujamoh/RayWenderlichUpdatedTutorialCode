978c30aaa2983e8591733e0edb1a1fa8e46546a0
qingqinghebiancao <1139513862@qq.com>
2015-11-12 14:58:44 +0800

    Finish the getting started with CoreData tutorial(swift)

--- HitList/HitList/ViewController.swift
+++ HitList/HitList/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController , UITableViewDataSource{
     
     @IBOutlet weak var tableView: UITableView!
     
     var names = [String]()
+    
+    var people = [NSManagedObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,13 +26,34 @@ class ViewController: UIViewController , UITableViewDataSource{
         title = "\"The List \""
         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // 1. Get the NSManagedObjectContext
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context = appDelegate.managedObjectContext
+        
+        // 2. Fetch the result from the context
+        let fetchRequest = NSFetchRequest(entityName: "Person")
+        
+        // 3. 
+        do {
+            let results = try
+                context.executeFetchRequest(fetchRequest)
+                people = results as! [NSManagedObject]
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+    }
 
     @IBAction func addName(sender: AnyObject) {
         let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .Alert)
         
         let saveAction = UIAlertAction(title: "Save", style: .Default, handler: {( action: UIAlertAction) -> Void in
             let textField = alert.textFields!.first
-            self.names.append(textField!.text!)
+//            self.names.append(textField!.text!)
+            self.saveName(textField!.text!)
             self.tableView.reloadData()
         })
         
@@ -46,17 +70,43 @@ class ViewController: UIViewController , UITableViewDataSource{
         presentViewController(alert, animated: true, completion: nil)
     }
     
-    // MARK: UITableViewDataSource
+    // MARK: - UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        //return names.count
+        return people.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell")
         
-        cell!.textLabel!.text = names[indexPath.row]
+//        cell!.textLabel!.text = names[indexPath.row]
+        let person = people[indexPath.row]
+        cell!.textLabel!.text = person.valueForKey("name") as? String
         
         return cell!
+    }
+    
+    // MARK: - Private methods
+    func saveName(name: String) {
+        // 1. Get the NSManagedObjectContext
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        // 2. Insert the object to the context
+        let entity = NSEntityDescription.entityForName("Person", inManagedObjectContext: managedContext)
+        
+        let person = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        
+        person.setValue(name, forKey: "name")
+        
+        do {
+            try managedContext.save()
+            
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save \(error), \(error.userInfo)")
+        }
     }
     
     override func didReceiveMemoryWarning() {
--- HitList/Keynote.md
+++ HitList/Keynote.md
@@ -1,1 +1,33 @@
-Tutorial source: [Getting Started with Core Data Tutorial](http://www.raywenderlich.com/115695/getting-started-with-core-data-tutorial)
+Tutorial source: [Getting Started with Core Data Tutorial][1]
+
+The key point of the tutorial:
+1. The step to insert objects to CoreData
+* Get the managed object context (NSManagedObjectContext)
+* Create a new managed object and insert it into the context above
+* Set the attributes of the managed object
+* Save the object to the context
+
+	func saveName(name: String) {
+		// 1. 
+	    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+	    let managedContext = appDelegate.managedObjectContext
+	        
+	     // 2. 
+	     let entity = NSEntityDescription.entityForName("Person", inManagedObjectContext: managedContext)
+	      let person = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+	      
+		// 3  
+	    person.setValue(name, forKey: "name")
+	    
+		// 4	
+	    do {
+	        try managedContext.save()  
+	        people.append(person)
+	    } catch let error as NSError {
+	        print("Could not save \(error), \(error.userInfo)")
+	    }
+	}
+	
+2. The step to fetch result from CoreData
+
+[1]:	http://www.raywenderlich.com/115695/getting-started-with-core-data-tutorial
