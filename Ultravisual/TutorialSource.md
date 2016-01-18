456b72cc738da669f9be45952fff294cdc7a0435
qingqinghebiancao <1139513862@qq.com>
2016-01-18 17:20:42 +0800

    Swift expanding cells using collection views

---
+++ Ultravisual/TutorialSource.md
@@ -0,0 +1,4 @@
+# Ultravisual
+Ultravisual-like Custom Collection View Layout
+
+http://www.raywenderlich.com/99087/swift-expanding-cells-ios-collection-views
---
+++ Ultravisual/Ultravisual/InspirationCell.swift
@@ -0,0 +1,40 @@
+//
+//  TutorialCell.swift
+//  RWDevCon
+//
+//  Created by Mic Pringle on 27/02/2015.
+//  Copyright (c) 2015 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+
+class InspirationCell: UICollectionViewCell {
+  
+  @IBOutlet private weak var imageView: UIImageView!
+  @IBOutlet private weak var imageCoverView: UIView!
+  
+  var inspiration: Inspiration? {
+    didSet {
+      if let inspiration = inspiration {
+        imageView.image = inspiration.backgroundImage
+      }
+    }
+  }
+    
+    override func applyLayoutAttributes(layoutAttributes: UICollectionViewLayoutAttributes) {
+        super.applyLayoutAttributes(layoutAttributes)
+        
+        // 1
+        let standardHeight = UltravisualLayoutConstants.Cell.standardHeight
+        let featuredHeight = UltravisualLayoutConstants.Cell.featuredHeight
+        
+        // 2
+        let delta = 1 - ((featuredHeight - CGRectGetHeight(frame)) / ( featuredHeight - standardHeight))
+        
+        // 3
+        let minAlpha: CGFloat = 0.3
+        let maxAlpha: CGFloat = 0.75
+        imageCoverView.alpha = maxAlpha - (delta * (maxAlpha - minAlpha))
+    }
+  
+}
